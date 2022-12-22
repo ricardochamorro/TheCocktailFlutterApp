93eeee6c4fdeb30add354516674b5b05d8be7314
Ricardo Chamorro <ricardojchamorroz@gmail.com>
2022-12-22 15:02:17 -0600

    Adding constants for views

---
+++ view/lib/src/common/constants/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const RCH_PrimaryColor = Color(0xFF6F35A5);
+const RCH_PrimaryLightColor = Color(0xFFF1E6FF);
+
+const RCH_SecondaryColor = Color(0xFFB5BFD0);
+const RCH_TextColor = Color(0xFF50505D);
+const RCH_TextLightColor = Color(0xFF6A727D);
+
+
+const RCH_ReadOnlyTextColor = Color(0xFF50505D);
