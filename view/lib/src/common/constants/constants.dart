f994d4777a1123d2ceed5d39868e74cbd2964fe5
Ricardo Chamorro <ricardojchamorroz@gmail.com>
2022-12-27 15:05:00 -0600

    Adding alphabet constants

--- view/lib/src/common/constants/constants.dart
+++ view/lib/src/common/constants/constants.dart
@@ -7,5 +7,32 @@ const RCH_SecondaryColor = Color(0xFFB5BFD0);
 const RCH_TextColor = Color(0xFF50505D);
 const RCH_TextLightColor = Color(0xFF6A727D);
 
-
 const RCH_ReadOnlyTextColor = Color(0xFF50505D);
+
+const List<String> alphabet = <String>[
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'Y',
+  'Z'
+];
