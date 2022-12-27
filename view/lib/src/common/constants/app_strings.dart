d7210cef5c7a7752710ccd49020e81de7b78e8d2
Ricardo Chamorro <ricardojchamorroz@gmail.com>
2022-12-27 15:03:39 -0600

    Adding new labels

--- view/lib/src/common/constants/app_strings.dart
+++ view/lib/src/common/constants/app_strings.dart
@@ -2,4 +2,9 @@ abstract class AppStrings {
   static const welcome = 'TheCocktailAPP';
 
   static const drinkScreen = 'Drink Detail';
+  static const cocktailName = 'Name';
+  static const cocktailCategory = 'Category';
+  static const cocktailIngredients = 'Ingredients';
+  static const cocktailGlass = 'Glass';
+  static const cocktailInstruction = 'Instruction';
 }
