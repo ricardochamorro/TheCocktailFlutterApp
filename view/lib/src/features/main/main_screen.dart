a8465e0111a0941522c35d531d3c931ab64f4f18
Ricardo Chamorro <ricardojchamorroz@gmail.com>
2022-12-27 15:01:31 -0600

    Modifying main screen to show COcktails per First Letter

--- view/lib/src/features/main/main_screen.dart
+++ view/lib/src/features/main/main_screen.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:core/core/core_screen.dart';
 import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,18 @@ import 'package:view/src/components/drinks_catalog.dart';
 import 'package:view/src/components/ingredients_cards_catalog.dart';
 import 'package:view/src/di/locator.dart';
 import 'package:view/src/features/main/main_view_model.dart';
+*/
+
+import 'package:core/core/core_screen.dart';
+import 'package:domain/domain.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:view/src/common/constants/app_strings.dart';
+import 'package:view/src/common/constants/constants.dart';
+import 'package:view/src/components/drinks_catalog.dart';
+import 'package:view/src/components/ingredients_cards_catalog.dart';
+import '../../di/locator.dart';
+import 'main_view_model.dart';
 
 class MainScreen extends CoreScreen<MainViewModel> {
   @override
@@ -21,17 +34,23 @@ class MainScreen extends CoreScreen<MainViewModel> {
         child: ListView(
       clipBehavior: Clip.none,
       children: [
-        DrinksCatalog<DrinkModel, DrinkModel>(
-          icon: Icons.abc,
-          title: "Letra V",
-          stream: viewModel.drinkByFirstLetter("V"),
-          onClick: viewModel.onClickDrinkCard,
-          scrollDirection: Axis.horizontal,
-        ),
-        IngredientsCardCatalog<IngredientModel>(
-            icon: Icons.abc,
-            title: "Select cocktails based on Ingredients",
-            stream: viewModel.allIngredientsUseCase()),
+        Expanded(
+            child: Container(
+                padding: const EdgeInsets.all(12),
+                child: ListView.builder(
+                    physics: ClampingScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: alphabet.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return DrinksCatalog<DrinkModel, DrinkModel>(
+                        icon: Icons.abc,
+                        title: '${alphabet[index]}',
+                        stream:
+                            viewModel.drinkByFirstLetter('${alphabet[index]}'),
+                        onClick: viewModel.onClickDrinkCard,
+                        scrollDirection: Axis.horizontal,
+                      );
+                    }))),
       ],
       scrollDirection: Axis.vertical,
     ));
